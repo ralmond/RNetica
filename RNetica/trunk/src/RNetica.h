5f8ea8522f8e7f0aa8ba1bcd1e7c4b09901c37c3
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2012-08-28 04:00:33 +0000

    System Stuff.

---
+++ RNetica/trunk/src/RNetica.h
@@ -0,0 +1,16 @@
+/**
+ * Header Files for R-Netica User Interface.
+ */
+
+/**
+ * This is a global pointer to the Netica environment.
+ * It is created once during a session.
+ */
+extern environ_ns* RN_netica_env;
+
+/**
+ * Common Symbols so we don't need to keep redefining them.
+ */
+extern char* NeticaClass;
+extern char* DeletedNeticaClass;
+extern char* NetPointer;
